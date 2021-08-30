b4a6a36125445bbaef9d8b5653f584737c79f06a
Armand Fourie <armand.4rie@gmail.com>
2021-08-30 17:41:35 +0200

    Added comments for understanding

--- lib/cubits/simple_calc/simple_calc_cubit.dart
+++ lib/cubits/simple_calc/simple_calc_cubit.dart
@@ -14,19 +14,19 @@ class SimpleCalcCubit extends Cubit<SimpleCalcState> {
     String msg = "";
 
     try {
-      // If tender is negative
+      //+ If tender is negative
       if (tender < 0) {
         msg = "The tender amount you entered was negative.";
-        tender *= -1;
-        totalChange = tender - cost;
+        totalChange = (tender * -1) - cost;
         tempChange = totalChange;
       } else {
-        // If tender is smaller than the cost amount.
+        //+ If tender is smaller than the cost amount.
         msg = (tender < cost)
             ? "Please check your amounts entered. The tender amount can't be smaller than the cost."
             : "";
       }
 
+      //+ Determining the denominations
       for (var denomination in validDenominations) {
         if (denomination <= tempChange) {
           int amount = tempChange ~/ denomination;
@@ -36,6 +36,7 @@ class SimpleCalcCubit extends Cubit<SimpleCalcState> {
         }
       }
 
+      //+ If the change given to the client is not precise
       msg = (tempChange > 0 && msg.isEmpty)
           ? "The rand note value was incorrect. Can't return: ${tempChange.toStringAsFixed(2)}c to client."
           : msg;
@@ -52,6 +53,7 @@ class SimpleCalcCubit extends Cubit<SimpleCalcState> {
     emit(SimpleCalcCalculated({}, 0, ""));
   }
 
+  //+ To change the state for the UI to allow change.
   void textError() {
     emit(SimpleCalcCalculated({}, 0, "Make sure that the values entered are correct."));
   }
